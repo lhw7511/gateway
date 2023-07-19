0199db091609f40da38eac9c53956198120e3bba
leehyeonuk <hwlee@beintech.co.kr>
2023-07-19 16:51:45 +0900

    cloud-config

--- src/main/java/com/msa/gateway/GatewayApplication.java
+++ src/main/java/com/msa/gateway/GatewayApplication.java
@@ -1,14 +1,23 @@
 package com.msa.gateway;
 
+import com.sun.net.httpserver.HttpExchange;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository(){
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
